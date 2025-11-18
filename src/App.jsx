144dda70968f39d3fe680da87fc8c34176b1f570
vetri <kulasekarapandiyan80@gmail.com>
2025-11-18 22:00:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import CertificationShowcase from './CertificationShowcase';
+import './style.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<CertificationShowcase />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
